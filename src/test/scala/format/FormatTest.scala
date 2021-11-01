b7a4a65f92bf284ad4d770d28d32c137a6104c4c
Herr Ritschwumm <ritschwumm@djini.de>
2021-11-01 17:05:43 +0100

    fix test output, update dependencies

--- src/test/scala/format/FormatTest.scala
+++ src/test/scala/format/FormatTest.scala
@@ -5,19 +5,19 @@ import minitest._
 import scutil.lang.implicits._
 
 object FormatTest extends SimpleTestSuite {
-	test("AudioFormat_S2LE should") {
+	test("AudioFormat_S2LE should work") {
 		assertEquals(
 			 AudioFormat_S2LE decode -32768,
 			 -1f
 		 )
 	}
-	test("AudioFormat_S2LE should") {
+	test("AudioFormat_S2LE should work") {
 		assertEquals(
 			 AudioFormat_S2LE decode 0,
 			 0f
 		 )
 	}
-	test("AudioFormat_S2LE should") {
+	test("AudioFormat_S2LE should work") {
 		assertEquals(
 			 AudioFormat_S2LE decode 16384,
 			 0.5f
@@ -25,31 +25,31 @@ object FormatTest extends SimpleTestSuite {
 	}
 	// 32768
 
-	test("AudioFormat_S2LE should") {
+	test("AudioFormat_S2LE should work") {
 		assertEquals(
 			 AudioFormat_S2LE encode -2f,
 			 -32768
 		 )
 	}
-	test("AudioFormat_S2LE should") {
+	test("AudioFormat_S2LE should work") {
 		assertEquals(
 			 AudioFormat_S2LE encode -1f,
 			 -32768
 		 )
 	}
-	test("AudioFormat_S2LE should") {
+	test("AudioFormat_S2LE should work") {
 		assertEquals(
 			 AudioFormat_S2LE encode 0f,
 			 0
 		 )
 	}
-	test("AudioFormat_S2LE should") {
+	test("AudioFormat_S2LE should work") {
 		assertEquals(
 			 AudioFormat_S2LE encode 1f,
 			 +32767.toShort
 		)
 	}
-	test("AudioFormat_S2LE should") {
+	test("AudioFormat_S2LE should work") {
 		assertEquals(
 			 AudioFormat_S2LE encode 2f,
 			 +32767.toShort
@@ -58,19 +58,19 @@ object FormatTest extends SimpleTestSuite {
 
 	//------------------------------------------------------------------------------
 
-	test("AudioFormat_U1 should") {
+	test("AudioFormat_U1 should work") {
 		assertEquals(
 			 AudioFormat_U1 decode 0,
 			 -1f
 		 )
 	}
-	test("AudioFormat_U1 should") {
+	test("AudioFormat_U1 should work") {
 		assertEquals(
 			 AudioFormat_U1 decode 128.toByte,
 			 0f
 		 )
 	}
-	test("AudioFormat_U1 should") {
+	test("AudioFormat_U1 should work") {
 		assertEquals(
 			 AudioFormat_U1 decode 192.toByte,
 			 0.5f
@@ -78,37 +78,37 @@ object FormatTest extends SimpleTestSuite {
 	}
 	// u255
 
-	test("AudioFormat_U1 should") {
+	test("AudioFormat_U1 should work") {
 		assertEquals(
 			 AudioFormat_U1 encode -2f,
 			 0
 		 )
 	}
-	test("AudioFormat_U1 should") {
+	test("AudioFormat_U1 should work") {
 		assertEquals(
 			 AudioFormat_U1 encode -1f,
 			 0
 		 )
 	}
-	test("AudioFormat_U1 should") {
+	test("AudioFormat_U1 should work") {
 		assertEquals(
 			 AudioFormat_U1 encode 0f,
 			 128.toByte
 		 )
 	}
-	test("AudioFormat_U1 should") {
+	test("AudioFormat_U1 should work") {
 		assertEquals(
 			 AudioFormat_U1 encode 0.5f,
 			 192.toByte
 		 )
 	}
-	test("AudioFormat_U1 should") {
+	test("AudioFormat_U1 should work") {
 		assertEquals(
 			 AudioFormat_U1 encode 1f,
 			 255.toByte
 		 )
 	}
-	test("AudioFormat_U1 should") {
+	test("AudioFormat_U1 should work") {
 		assertEquals(
 			 AudioFormat_U1 encode 2f,
 			 255.toByte
@@ -117,32 +117,32 @@ object FormatTest extends SimpleTestSuite {
 
 	//------------------------------------------------------------------------------
 
-	test("AudioFormat_S3LE should") {
+	test("AudioFormat_S3LE should work") {
 		assertEquals(
 			 AudioFormat_S3LE.toInt(byte"56", byte"34", byte"12"),
 			 int"00123456"
 		 )
 	}
-	test("AudioFormat_S3LE should") {
+	test("AudioFormat_S3LE should work") {
 		assertEquals(
 			 AudioFormat_S3LE.toInt(byte"ab", byte"cd", byte"ef"),
 			 int"ffefcdab"
 		 )
 	}
 
-	test("AudioFormat_S3LE should") {
+	test("AudioFormat_S3LE should work") {
 		assertEquals(
 			 AudioFormat_S3LE.decode(byte"00", byte"00", byte"00"),
 			 0f
 		 )
 	}
-	test("AudioFormat_S3LE should") {
+	test("AudioFormat_S3LE should work") {
 		assertEquals(
 			 AudioFormat_S3LE.decode(byte"00", byte"00", byte"80"),
 			 -1f
 		 )
 	}
-	test("AudioFormat_S3LE should") {
+	test("AudioFormat_S3LE should work") {
 		assertEquals(
 			 AudioFormat_S3LE.decode(byte"00", byte"00", byte"40"),
 			 0.5f
